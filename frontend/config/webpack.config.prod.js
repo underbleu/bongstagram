92147eb019d539112cbc4f4d5258afe90983b796
bong kim <32095134+underbleu@users.noreply.github.com>
2018-04-04 00:34:41 +0900

    개발 설정 - scss to webpack (배포용: 독립된 css파일 생성)

--- frontend/config/webpack.config.prod.js
+++ frontend/config/webpack.config.prod.js
@@ -166,7 +166,7 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /\.(css|scss)$/,
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
@@ -203,8 +203,15 @@ module.exports = {
                             flexbox: 'no-2009',
                           }),
                         ],
+                        sourceMap: true
                       },
                     },
+                    {
+                      loader: require.resolve('sass-loader'),
+                      options: {
+                        sourseMap: true
+                      }
+                    }
                   ],
                 },
                 extractTextPluginOptions
