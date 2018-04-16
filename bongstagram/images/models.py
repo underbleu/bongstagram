d7849edac3c52f571a81b85aaf7007f9b5ae7c3e
underbleu <underbleu.dev@gmail.com>
2018-04-16 14:54:30 +0900

    개발 설정 - Like모델 생성 / Image,Comment모델에 creator와 image 변수 추가 & Migration

--- bongstagram/images/models.py
+++ bongstagram/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from bongstagram.users import models as user_models
 
 
 class TimeStampedModel(models.Model):
@@ -12,11 +13,26 @@ class TimeStampedModel(models.Model):
 
 class Image(TimeStampedModel):
 
+    """ Image Model """
+
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
 
 
 class Comment(TimeStampedModel):
 
+    """ Comment Model """
+
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+
+class Like(TimeStampedModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
