982c3a385b6e1f6fb9fc452093d5909236bba46a
underbleu <underbleu.dev@gmail.com>
2018-04-18 15:30:31 +0900

    백엔드 - Comment, Like View 생성, Url연결, 등록

--- bongstagram/images/views.py
+++ bongstagram/images/views.py
@@ -4,10 +4,32 @@ from . import models, serializers
 
 class ListAllImages(APIView):
 
-    def get(self, request, format=None):
+    def get(self, request, format=None): # GET method
         
         all_images = models.Image.objects.all() # DB에 있는 모든 이미지 가져오기
         
         serializer = serializers.ImageSerializer(all_images, many=True) # python -> json 오브젝트로 변환하여 data에 저장
         
         return Response(data=serializer.data)
+
+
+class ListAllComments(APIView):
+    
+    def get(self, request, format=None):
+        
+        all_comments = models.Comment.objects.all()
+        
+        serializer = serializers.CommentSerializer(all_comments, many=True)
+        
+        return Response(data=serializer.data)
+
+
+class ListAllLikes(APIView):
+    
+    def get(self, request, format=None):
+        
+        all_likes = models.Like.objects.all()
+        
+        serializer = serializers.LikeSerializer(all_likes, many=True)
+        
+        return Response(data = serializer.data)
