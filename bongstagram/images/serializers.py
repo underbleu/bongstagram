aa06ba56074dec3c19ee051e0041697caac858ba
underbleu <underbleu.dev@gmail.com>
2018-04-20 01:08:09 +0900

    기능 추가 - FeedUserSerializer 생성
    creator정보를 id로 표기하던 것에서 profile_image와 username으로 표기

--- bongstagram/images/serializers.py
+++ bongstagram/images/serializers.py
@@ -1,26 +1,49 @@
 from rest_framework import serializers
 from . import models
+from bongstagram.users import models as user_model
 
-class ImageSerializer(serializers.ModelSerializer):
+class FeedUserSerializer(serializers.ModelSerializer):
     
     class Meta:
-        model = models.Image
-        fields = '__all__'
+        model = user_model.User
+        fields = (
+            'username',
+            'profile_image',
+        )
 
 
 class CommentSerializer(serializers.ModelSerializer):
 
-    image = ImageSerializer()
+    creator = FeedUserSerializer()
     
     class Meta:
         model = models.Comment
-        fields = '__all__'
+        fields = (
+            'id',
+            'message',
+            'creator'
+        )
 
 
 class LikeSerializer(serializers.ModelSerializer):
     
-    image = ImageSerializer()
-    
     class Meta:
         model = models.Like
         fields = '__all__'
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    
+    comments = CommentSerializer(many=True) # comments: backward relationship from Comment Model
+    creator = FeedUserSerializer()
+    
+    class Meta:
+        model = models.Image
+        fields = (
+            'id',
+            'file',
+            'location',
+            'caption',
+            'comments',
+            'creator'
+        )
