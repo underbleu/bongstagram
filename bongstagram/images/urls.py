787d079d80dab70a1b2d6a86309d2f44acb9d022
underbleu <underbleu.dev@gmail.com>
2018-04-19 21:11:34 +0900

    리팩토링 - Image urls. views의 테스트코드 제거

--- bongstagram/images/urls.py
+++ bongstagram/images/urls.py
@@ -4,18 +4,5 @@ from . import views
 app_name = "images" # Django 2.0 need it.
 urlpatterns = [
     url(
-        regex=r"^all/$",
-        view=views.ListAllImages.as_view(),
-        name="all_images"
     ),
-    url(
-        regex=r"^comments/$",
-        view=views.ListAllComments.as_view(),
-        name="all_comments"
-    ),
-    url(
-        regex=r"^likes/$",
-        view=views.ListAllLikes.as_view(),
-        name="all_likes"
-    )
 ]
