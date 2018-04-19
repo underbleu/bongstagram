8b79fab6c15f2616029fd84426571e09998b6a62
underbleu <underbleu.dev@gmail.com>
2018-04-19 21:13:49 +0900

    기능 추가 - Feed 모델의 view, url 생성

--- bongstagram/images/urls.py
+++ bongstagram/images/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = "images" # Django 2.0 need it.
 urlpatterns = [
     url(
+        regex=r"^$",
+        view=views.Feed.as_view(),
+        name="feed"
     ),
 ]
--- bongstagram/images/views.py
+++ bongstagram/images/views.py
@@ -2,11 +2,26 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import models, serializers
 
+class Feed(APIView):
     
     def get(self, request, format=None):
         
+        user = request.user
         
+        following_users = user.following.all()
         
+        image_list = []
         
+        for following_user in following_users:
+            
+            user_images = following_user.images.all()[:2]
+            
+            for image in user_images:
+                
+                image_list.append(image)
         
+        sorted_images = sorted(image_list, key=lambda image: image.created_at, reverse=True)
         
+        serializer = serializers.ImageSerializer(sorted_images, many=True)
+        
+        return Response(serializer.data)
