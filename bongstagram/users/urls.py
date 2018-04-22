78293bfd4b13fda78ba611c75928fdb446ca2d2b
underbleu <underbleu.dev@gmail.com>
2018-04-22 22:19:47 +0900

    기능추가 - Follow / Unfollow 기능

--- bongstagram/users/urls.py
+++ bongstagram/users/urls.py
@@ -2,11 +2,21 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = "users"
+app_name = 'users'
 urlpatterns = [
     url(
-        regex=r"^explore/$",
+        regex=r'^explore/$',
         view=views.ExploreUsers.as_view(),
-        name="explore_users",
+        name='explore_users',
+    ),
+    url(
+        regex=r'(?P<user_id>[0-9]+)/follow/$',
+        view=views.FollowUser.as_view(),
+        name='follow_user',
+    ),
+    url(
+        regex=r'(?P<user_id>[0-9]+)/unfollow/$',
+        view=views.UnFollowUser.as_view(),
+        name='unfollow_user',
     ),
 ]
