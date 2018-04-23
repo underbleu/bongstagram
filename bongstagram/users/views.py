5b8224b14bbc1aa57b7ed742c476b4f51018fd48
underbleu <underbleu.dev@gmail.com>
2018-04-24 06:00:02 +0900

    리팩토링 - ListUserSerializer: 명확한 이름으로 시리얼라이저 이름 수정

--- bongstagram/users/views.py
+++ bongstagram/users/views.py
@@ -23,7 +23,7 @@ class ExploreUsers(APIView):
         
         last_five = models.User.objects.all().order_by('-date_joined')[:5]
         
-        serializer = serializers.ExploreUserSerializer(last_five, many=True)
+        serializer = serializers.ListUserSerializer(last_five, many=True)
         
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
