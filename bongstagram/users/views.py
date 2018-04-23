c46bdfc02e3fcc47b434ebe5164561fb24200c51
underbleu <underbleu.dev@gmail.com>
2018-04-24 06:04:09 +0900

    리팩토링 - 기능 만든 순서대로 코드정렬

--- bongstagram/users/views.py
+++ bongstagram/users/views.py
@@ -3,20 +3,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from . import models, serializers
 
-class UserProfile(APIView):
-    
-    def get(self, request, username, format=None):
-        
-        try:
-            found_user = models.User.objects.get(username=username)
-        except models.User.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = serializers.UserProfileSerializer(found_user)
-        
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-
 class ExploreUsers(APIView):
     
     def get(self, request, format=None):
@@ -62,3 +48,18 @@ class UnFollowUser(APIView):
         user.save()
         
         return Response(status=status.HTTP_200_OK)
+
+
+class UserProfile(APIView):
+    
+    def get(self, request, username, format=None):
+        
+        try:
+            found_user = models.User.objects.get(username=username)
+        except models.User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = serializers.UserProfileSerializer(found_user)
+        
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
