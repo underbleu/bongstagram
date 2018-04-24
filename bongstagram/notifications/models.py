38ff756d0945b6a3294ee5b1946063ce0b816743
underbleu <underbleu.dev@gmail.com>
2018-04-25 01:06:00 +0900

    기능개선 - notification 정렬기준, string representation 설정

--- bongstagram/notifications/models.py
+++ bongstagram/notifications/models.py
@@ -14,3 +14,10 @@ class Notification(image_models.TimeStampedModel):
     to = models.ForeignKey(user_models.User, related_name='to', on_delete=models.PROTECT)
     notification_type = models.CharField(max_length=20, choices=TYPE_CHOICES, null=True)
     image = models.ForeignKey(image_models.Image, null=True, blank=True, on_delete=models.PROTECT)
+    comment = models.TextField(null=True, blank=True) # comment 요약정보. Like와 Follow에선 필수아님
+    
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return 'From: {} - To: {}'.format(self.creator, self.to)
