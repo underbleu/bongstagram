d0542abf0c1635e2dbc9ea11e6caa89549dad103
underbleu <underbleu.dev@gmail.com>
2018-04-26 15:07:15 +0900

    리팩토링 - 중복코드 함수화 get_user

--- bongstagram/users/views.py
+++ bongstagram/users/views.py
@@ -56,17 +56,26 @@ class UnFollowUser(APIView):
 
 class UserProfile(APIView):
     
-    def get(self, request, username, format=None):
+    def get_user(self, username):
         
         try:
             found_user = models.User.objects.get(username=username)
+            return found_user
         except models.User.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return None
+    
+    def get(self, request, username, format=None):
         
+        found_user = self.get_user(username)
+        
+        if found_user is None:
+            
+            return Response(status.status.HTTP_404_NOT_FOUND)
+            
         serializer = serializers.UserProfileSerializer(found_user)
         
         return Response(data=serializer.data, status=status.HTTP_200_OK)
-
+        found_user = self.get_user(username)
 
 class UserFollowers(APIView):
     
