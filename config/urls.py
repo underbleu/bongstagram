f57c202f52dc6fae9e74ab067555b12f8a1a8843
underbleu <underbleu.dev@gmail.com>
2018-04-29 06:51:13 +0900

    개발설정 - 장고서버에서 react-app-view를 보여주도록 설정
    1. base.py: 장고에게 리액트 static파일이 frontend/build안에 있다고 알려줌
    2. views.py: frontend / build / index.html로 뷰생성
    3. urls.py: catch-all URL생성

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
 from rest_framework_jwt.views import obtain_jwt_token
+from bongstagram import views
 
 urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
@@ -13,12 +14,12 @@ urlpatterns = [
     # User management
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
-    # url(r'^api-token-auth/', obtain_jwt_token), # 충돌방지를위해 삭제
     url(r"^users/", include("bongstagram.users.urls", namespace="users")),
     url(r"^images/", include("bongstagram.images.urls", namespace="images")),
     url(r"^notifications/", include("bongstagram.notifications.urls", namespace="notifications")),
     url(r"^accounts/", include("allauth.urls")),
-    # Your stuff: custom urls includes go here
+    url(r"^", views.ReactAppView.as_view()),
+    
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
