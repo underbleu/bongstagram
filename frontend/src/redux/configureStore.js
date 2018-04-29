32f0241b3bcaceaa7d829c7615613d553eefead0
underbleu <underbleu.dev@gmail.com>
2018-04-29 23:43:35 +0900

    개발설정 - 개발환경에서만 사용할 미들웨어 적용

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -2,12 +2,19 @@ import { createStore, combine, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import users from "redux/modules/users";
 
-const middlewares = [thunk];
+const env = process.env.NODE_ENV; // 코드실행 환경출력 (dev/prod)
+
+
+if(env === "development"){
+  const { logger } = require("redux-logger");
+  middlewares.push(logger)
+}
 
 const reducer = combineReducers({
   users
 })
 
-let store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+let store = initialState => 
+  createStore(reducer, applyMiddleware(...middlewares));
 
 export default store();
