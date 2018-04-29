2374aa7690d7ab3e95bf54388952d4d5579a354d
underbleu <underbleu.dev@gmail.com>
2018-04-30 00:20:20 +0900

    기능추가 -  Syncing React-router with Redux

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,8 +1,14 @@
 import { createStore, combine, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { routerReducer, routerMiddleware } from "react-router-redux";
+import createHistory from "history/createBrowserHistory"
 import users from "redux/modules/users";
 
 const env = process.env.NODE_ENV; // 코드실행 환경출력 (dev/prod)
+
+const history = createHistory();
+
+const middlewares = [thunk, routerMiddleware(history)]; // 리덕스와 history를 싱크시키기
 
 
 if(env === "development"){
@@ -11,10 +17,13 @@ if(env === "development"){
 }
 
 const reducer = combineReducers({
-  users
+  users, 
+  routing: routerReducer
 })
 
 let store = initialState => 
   createStore(reducer, applyMiddleware(...middlewares));
 
+export { history };
+  
 export default store();
