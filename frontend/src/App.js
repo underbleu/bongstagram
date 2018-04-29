87a424523a7509b7f4b3ec8df655d3fc5c9661c0
underbleu <underbleu.dev@gmail.com>
2018-04-29 20:10:28 +0900

    개발설정 - Redux 설치, 불필요파일 제거

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import styles from './App.scss';
 
 class App extends Component {
   render() {
-    return (
-      <div className={styles.App}>
-        <header className={styles.App__header}>
-          <img src={logo} className={styles.App__logo} alt="logo" />
-          <h1 className={styles.App__title}>Welcome to React</h1>
-        </header>
-        <p className={styles.App__intro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div className={styles.App} />;
   }
 }
 
