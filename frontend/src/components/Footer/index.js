91c880dc9667d830c642c241d2658c64016d10d3
underbleu <underbleu.dev@gmail.com>
2018-05-02 11:28:38 +0900

    Footer 스타일링

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import styles from "./styles.scss"
 
 const Footer = (props, context) => (
   <footer className={styles.footer}>
-    <div className={styles.column}>
+    <div className={styles.wrapper}>
       <nav className={styles.nav}>
         <ul className={styles.list}>
           <li className={styles.listItem}>About Us</li>
@@ -20,8 +20,6 @@ const Footer = (props, context) => (
           <li className={styles.listItem}>Language</li>
         </ul>
       </nav>
-    </div>
-    <div className={styles.column}>
       <span className={styles.copyright}>&copy; 2018 Bongstagram</span>
     </div>
   </footer>
