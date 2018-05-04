9a04d2c6e23c6f4bf4d0298f8f4902c93064a66a
underbleu <underbleu.dev@gmail.com>
2018-05-04 13:14:30 +0900

    App 컴포넌트 라우팅

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -1,15 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import styles from "./styles.scss";
 import Footer from "components/Footer";
 
-class App extends Component {
-  render() {
-    return (
-      <div className={styles.App}>
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = props => [
+  // 1. Nav
+  // 2. Routes
+  props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
+  // 3. Footer
+  <Footer key={3} />
+];
+
+const PrivateRoutes = props => (
+  <Switch>
+    <Route exact path="/" render={() => "feed"} />
+    <Route path="/explore" render={() => "explore "} />
+  </Switch>
+);
+
+const PublicRoutes = props => (
+  <Switch>
+    <Route path="/login" render={() => "login"} />
+    <Route path="/forget" render={() => "password"} />
+  </Switch>
+);
 
 export default App;
