d9f4e35e03fcc2dc34e2fff47106182c40406929
underbleu <underbleu.dev@gmail.com>
2018-05-09 02:16:05 +0900

    context의 t(): 1. Stateful 컴포넌트 번역하기

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,28 +1,39 @@
 import React from "react";
-import styles from "./styles.scss"
+import PropTypes from "prop-types";
+import styles from "./styles.scss";
 
-const Footer = (props, context) => (
-  <footer className={styles.footer}>
-    <div className={styles.wrapper}>
-      <nav className={styles.nav}>
-        <ul className={styles.list}>
-          <li className={styles.listItem}>About Us</li>
-          <li className={styles.listItem}>Support</li>
-          <li className={styles.listItem}>Blog</li>
-          <li className={styles.listItem}>Press</li>
-          <li className={styles.listItem}>API</li>
-          <li className={styles.listItem}>Jobs</li>
-          <li className={styles.listItem}>Privacy</li>
-          <li className={styles.listItem}>Terms</li>
-          <li className={styles.listItem}>Directory</li>
-          <li className={styles.listItem}>Profiles</li>
-          <li className={styles.listItem}>Hashtags</li>
-          <li className={styles.listItem}>Language</li>
-        </ul>
-      </nav>
-      <span className={styles.copyright}>&copy; 2018 Bongstagram</span>
-    </div>
-  </footer>
-);
+class Footer extends React.Component {
+  
+  static contextTypes = {
+    t: PropTypes.func.isRequired
+  }
+  
+  render() {
+    return (
+      <footer className={styles.footer}>
+        <div className={styles.wrapper}>
+          <nav className={styles.nav}>
+            <ul className={styles.list}>
+              <li className={styles.listItem}>{this.context.t("About Us")}</li>
+              <li className={styles.listItem}>{this.context.t("Support")}</li>
+              <li className={styles.listItem}>{this.context.t("Blog")}</li>
+              <li className={styles.listItem}>{this.context.t("Press")}</li>
+              <li className={styles.listItem}>{this.context.t("API")}</li>
+              <li className={styles.listItem}>{this.context.t("Jobs")}</li>
+              <li className={styles.listItem}>{this.context.t("Privacy")}</li>
+              <li className={styles.listItem}>{this.context.t("Terms")}</li>
+              <li className={styles.listItem}>{this.context.t("Directory")}</li>
+              <li className={styles.listItem}>{this.context.t("Profiles")}</li>
+              <li className={styles.listItem}>{this.context.t("Hashtags")}</li>
+              <li className={styles.listItem}>{this.context.t("Language")}</li>
+            </ul>
+          </nav>
+          <span className={styles.copyright}>&copy; 2018 Bongstagram</span>
+        </div>
+      </footer>
+    );
+  }
+} 
+
 
 export default Footer;
