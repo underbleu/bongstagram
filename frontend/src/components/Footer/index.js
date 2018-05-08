d9e4a99eb3a4657899bc2e7883a59fe56197b2bd
underbleu <underbleu.dev@gmail.com>
2018-05-09 02:17:00 +0900

    context의 t(): 2. Stateless 컴포넌트 번역하기

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -2,38 +2,32 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./styles.scss";
 
-class Footer extends React.Component {
-  
-  static contextTypes = {
-    t: PropTypes.func.isRequired
-  }
-  
-  render() {
-    return (
-      <footer className={styles.footer}>
-        <div className={styles.wrapper}>
-          <nav className={styles.nav}>
-            <ul className={styles.list}>
-              <li className={styles.listItem}>{this.context.t("About Us")}</li>
-              <li className={styles.listItem}>{this.context.t("Support")}</li>
-              <li className={styles.listItem}>{this.context.t("Blog")}</li>
-              <li className={styles.listItem}>{this.context.t("Press")}</li>
-              <li className={styles.listItem}>{this.context.t("API")}</li>
-              <li className={styles.listItem}>{this.context.t("Jobs")}</li>
-              <li className={styles.listItem}>{this.context.t("Privacy")}</li>
-              <li className={styles.listItem}>{this.context.t("Terms")}</li>
-              <li className={styles.listItem}>{this.context.t("Directory")}</li>
-              <li className={styles.listItem}>{this.context.t("Profiles")}</li>
-              <li className={styles.listItem}>{this.context.t("Hashtags")}</li>
-              <li className={styles.listItem}>{this.context.t("Language")}</li>
-            </ul>
-          </nav>
-          <span className={styles.copyright}>&copy; 2018 Bongstagram</span>
-        </div>
-      </footer>
-    );
-  }
-} 
+const Footer = (props, context) => (
+  <footer className={styles.footer}>
+    <div className={styles.wrapper}>
+      <nav className={styles.nav}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>{context.t("About Us")}</li>
+          <li className={styles.listItem}>{context.t("Support")}</li>
+          <li className={styles.listItem}>{context.t("Blog")}</li>
+          <li className={styles.listItem}>{context.t("Press")}</li>
+          <li className={styles.listItem}>{context.t("API")}</li>
+          <li className={styles.listItem}>{context.t("Jobs")}</li>
+          <li className={styles.listItem}>{context.t("Privacy")}</li>
+          <li className={styles.listItem}>{context.t("Terms")}</li>
+          <li className={styles.listItem}>{context.t("Directory")}</li>
+          <li className={styles.listItem}>{context.t("Profiles")}</li>
+          <li className={styles.listItem}>{context.t("Hashtags")}</li>
+          <li className={styles.listItem}>{context.t("Language")}</li>
+        </ul>
+      </nav>
+      <span className={styles.copyright}>&copy; 2018 Bongstagram</span>
+    </div>
+  </footer>
+);
 
+Footer.contextTypes = {
+  t: PropTypes.func.isRequired // --- 1. context의 t함수 불러오기
+};
 
 export default Footer;
