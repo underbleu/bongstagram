5133310da8e4ea18562fe871d4fb0864f4460e62
underbleu <underbleu.dev@gmail.com>
2018-05-09 13:35:31 +0900

    i18n패키지의 extract, import함수를 이용하여 번역하기 (자동식)

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,16 +1,18 @@
 export const translations = {
-  ko: {
-    "About Us": "BONGSTAGRAM 정보",
-    "Support": "지원",
-    "Blog": "블로그",
-    "Press": "홍보 센터",
-    "API": "API",
-    "Jobs": "채용",
-    "Privacy": "개인정보처리방침",
-    "Terms": "약관",
-    "Directory": "디렉터리",
-    "Profiles": "프로필",
-    "Hashtags": "해시태그",
-    "Language": "언더"
-  }
+  'ko': {
+    'About Us': 'BONGSTAGRAM정보',
+    'Support': '지원',
+    'Blog': '블로그',
+    'Press': '홍보 센터',
+    'API': 'API',
+    'Jobs': '채용',
+    'Privacy': '개인정보처리방침',
+    'Terms': '약관',
+    'Directory': '디렉터리',
+    'Profiles': '프로필',
+    'Hashtags': '해시태그',
+    'Language': '언어',
+  },
+  'options': {
+  },
 }
