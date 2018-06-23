89c2ef7a5c123a8b9885c8844a503f694af29169
underbleu <underbleu.dev@gmail.com>
2018-06-24 01:14:32 +0900

    Transfer 컨트랙트 함수 프론트에 구현 2

--- frontend/src/redux/modules/token.js
+++ frontend/src/redux/modules/token.js
@@ -1,10 +1,51 @@
-// import 
+// import
+import store from "redux/configureStore";
 
+// Web3
+import abiArray from "build/contracts/CopyrightToken.json";
+import Web3 from "web3";
+
+// Meta Mask
+const web3 = window.web3;
+const MyContract = web3.eth.contract(abiArray.abi);
+const contractInstance = MyContract.at(
+  "0x71b0eff316717a42bde1050c2e371debbfe74dcb"
+); // -> 디플로이된 CopyrightToken의 컨트랙트 주소
+
+// web3 1.0
+const web3socket = window.web3socket = new Web3(new Web3.providers.WebsocketProvider('ws://13.125.208.193:8546'));
+const MyContract2 = window.MyContract2 = new web3socket.eth.Contract(abiArray.abi, "0x71b0eff316717a42bde1050c2e371debbfe74dcb");
+
+// const dispatchSaveImageToken = (imageId, photoToken) => store.dispatch(saveImageToken(imageId, photoToken));
+
+MyContract2.events
+  .Transfer((err, event) => { if(err) console.log(err) })
+  .on('data', function(event){
+    console.log(event, 'from Transfer Event!')
+    const from = event.returnValues._from;
+    const to = event.returnValues._to;
+    const photoToken = event.returnValues._tokenId;
+    alert(`Copyright No. ${photoToken}'s ownership has transfered from ${from} to ${to} !`);
+  })
+  .on('error', function(error){
+    console.log(error)
+  });
 
 // action
 const WALLET_LOADING = "WALLET_LOADING";
 
 // action creator
+
+function transferCopyright(address, photoToken, imageId, gas) {
+  contractInstance.transfer.sendTransaction(
+    address, photoToken, imageId,
+    { from: getState().token.walletAddress, gas: `${gas}` },
+    (err, txHash) => err ? console.log(err) : dispatch(saveTxHash(imageId, txHash));
+  );
+}
+
+
+
 function walletLoading(walletAddress) {
   return {
     type: WALLET_LOADING,
@@ -51,10 +92,8 @@ function applyWalletLoading(state, action) {
 
 // export
 const actionCreators = {
-  getWallet
+  getWallet,
+  transferCopyright
 }
 
 export { actionCreators }
-
-export default reducer;
-
